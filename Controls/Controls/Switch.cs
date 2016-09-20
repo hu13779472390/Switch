8d8ec38561762a95c82e4f19d1251224be46c9fc
Jase <public@jase.online>
2016-09-20 12:08:54 +1000

    Handled OnPaint event

--- Controls/Controls/Switch.cs
+++ Controls/Controls/Switch.cs
@@ -110,7 +110,72 @@ namespace Controls
 
         protected override void OnPaint(PaintEventArgs pe)
         {
-            base.OnPaint(pe);
+            Graphics surface = pe.Graphics;
+
+            SolidBrush switchBrush = new SolidBrush(Color.FromArgb(14, 134, 201));
+
+            switch (CurrentState)
+            {
+                case SwitchStates.On:
+                    if (mouseEntered)
+                    {
+                        if (mouseDown)
+                        {
+                            this.BackColor = Color.FromArgb(201, 224, 235);
+
+                            surface.DrawRectangle(new Pen(Color.FromArgb(106, 185, 232), 1), 0, 0, this.Width - 1, this.Height - 1);
+                            surface.FillRectangle(switchBrush, (this.Width - 17), 0, 15, this.Height);
+                        }
+                        else
+                        {
+                            this.BackColor = Color.FromArgb(201, 224, 235);
+                            switchBrush = new SolidBrush(Color.FromArgb(24, 141, 211));
+
+                            surface.DrawRectangle(new Pen(Color.FromArgb(106, 185, 232), 1), 0, 0, this.Width - 1, this.Height - 1);
+                            surface.FillRectangle(switchBrush, (this.Width - 17), 0, 15, this.Height);
+                        }
+                    }
+                    else
+                    {
+                        this.BackColor = Color.FromArgb(211, 234, 248);
+                        switchBrush = new SolidBrush(Color.FromArgb(34, 151, 221));
+
+                        surface.DrawRectangle(new Pen(Color.FromArgb(106, 185, 232), 1), 0, 0, this.Width - 1, this.Height - 1);
+                        surface.FillRectangle(switchBrush, (this.Width - 17), 0, 15, this.Height);
+                    }
+                    break;
+                case SwitchStates.Off:
+                    if (mouseEntered)
+                    {
+                        if (mouseDown)
+                        {
+                            this.BackColor = Color.FromArgb(238, 217, 214);
+                            switchBrush = new SolidBrush(Color.FromArgb(185, 63, 45));
+
+                            surface.DrawRectangle(new Pen(Color.FromArgb(210, 123, 110), 1), 0, 0, this.Width - 1, this.Height - 1);
+                            surface.FillRectangle(switchBrush, 2, 0, 15, this.Height);
+                        }
+                        else
+                        {
+                            this.BackColor = Color.FromArgb(238, 217, 214);
+                            switchBrush = new SolidBrush(Color.FromArgb(205, 83, 65));
+
+                            surface.DrawRectangle(new Pen(Color.FromArgb(210, 123, 110), 1), 0, 0, this.Width - 1, this.Height - 1);
+                            surface.FillRectangle(switchBrush, 2, 0, 15, this.Height);
+                        }
+                    }
+                    else
+                    {
+                        this.BackColor = Color.FromArgb(248, 227, 224);
+                        switchBrush = new SolidBrush(Color.FromArgb(205, 83, 65));
+
+                        surface.DrawRectangle(new Pen(Color.FromArgb(220, 133, 120), 1), 0, 0, this.Width - 1, this.Height - 1);
+                        surface.FillRectangle(switchBrush, 2, 0, 15, this.Height);
+                    }
+                    break;
+            }
+
+            switchBrush.Dispose();
         }
     }
 }
